143d424050a0ddd485ccab7b746cd255bf6899b1
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-23 15:07:05 +0200

    ajout tests

--- src/test/java/junits/StringUtilsTest.java
+++ src/test/java/junits/StringUtilsTest.java
@@ -1,4 +1,5 @@
 package junits;
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -14,6 +15,18 @@ public class StringUtilsTest {
 	}
 
 	@Test
+	public void testEval2() {
+		int res = StringUtils.levenshteinDistance("distance", "instance");
+		assertEquals(2, res);
+	}
+
+	@Test
+	public void testEval3() {
+		int res = StringUtils.levenshteinDistance("aviron", "avion");
+		assertEquals(1, res);
+	}
+
+	@Test
 	public void testEvalAvecEspaces() {
 		int res = StringUtils.levenshteinDistance("Chien  ", "Chine");
 		assertEquals(2, res);
@@ -24,7 +37,7 @@ public class StringUtilsTest {
 		int res = StringUtils.levenshteinDistance(null, null);
 		assertEquals(0, res);
 	}
-	
+
 	@Test
 	public void testTrim() {
 		CharSequence c = StringUtils.trim("   test  ");
